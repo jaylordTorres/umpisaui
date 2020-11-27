8a58e873e6b571d50feed30c26a0916d21e93df1
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-11-27 20:34:53 +0800

    TextField hintText

--- lib/rental/RuiTextFieldStories.dart
+++ lib/rental/RuiTextFieldStories.dart
@@ -7,14 +7,14 @@ List<Story> ruiTextFieldStories = [
   Story(
     name: "${RuiTextField.name}",
     builder: (_, k) => RuiTextField(
-      labelText: k.text('labelText', initial: "Confirm Password"),
-      type: k.options(
-        'type',
-        initial: RuiTextFieldType.underlinePrimary,
-        options: RuiTextFieldType.values
-            .map((type) => Option(type.toString(), type))
-            .toList(),
-      ),
-    ),
-  )
+        labelText: k.text('labelText', initial: "Confirm Password"),
+        type: k.options(
+          'type',
+          initial: RuiTextFieldType.underlinePrimary,
+          options: RuiTextFieldType.values
+              .map((type) => Option(type.toString(), type))
+              .toList(),
+        ),
+        hintText: k.text('hintText', initial: "Enter here...")),
+  ),
 ];
