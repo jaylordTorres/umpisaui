40699376535ca6010dce10d2478f1bf52f5f4a9b
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-11-27 20:34:14 +0800

    fix size

--- lib/ui/constants/fontSizes.dart
+++ lib/ui/constants/fontSizes.dart
@@ -1,7 +1,7 @@
 // colors collections
 class UiFontSizes {
   static final double xl = 50.0;
-  static final double lg = 40.0;
+  static final double lg = 20.0;
   static final double md = 14.0;
   static final double sm = 12.0;
   static final double xs = 10.0;
